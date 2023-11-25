1002528040877402d43f7e0223ef345d339f8366
DJyakoff <149240980+DJyakoff@users.noreply.github.com>
2023-11-25 13:03:29 +0300

    First exercise

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿// // Задание №1
+// // Задайте одномерный массив из 10 целых чисел от 1 до 100. 
+// // Найдите количество элементов массива, 
+// // значения которых лежат в отрезке [20,90].
+// //не очень понял включительно ли 90, сделал включительно
+// using System.Collections;
+
+// int[] array = new int [10];
+// int searchNumber = 0;
+// for (int i = 0; i<10; i++)
+// {
+//     array[i]= new Random().Next(1, 101);
+//     if (array[i] >= 20 && array[i] <= 90)
+//     searchNumber +=1;
+// }
+// Console.WriteLine ($"Заданный массив: [{string.Join("; ", array)}]");
+// Console.WriteLine (searchNumber);
